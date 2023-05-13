ab7c3c17d080e0f00bcefb0dc392d517b6846acf
Marko Melle <markmelle1@gmail.com>
2023-05-13 13:14:22 +0200

    linted

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,26 +11,26 @@ function App() {
     backgroundColor: '#282c34',
   };
   return (
-    <div className='App' style={appStyle}>
-      <Card title='Card title' img='https://picsum.photos/200/300'>
-        <p className='card-text'>
+    <div className="App" style={appStyle}>
+      <Card title="Card title" img="https://picsum.photos/200/300">
+        <p className="card-text">
           Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+          bulk of the card&#39;s content.
         </p>
-        <a href='#' className='btn btn-primary'>
+        <a href="#root" className="btn btn-primary">
           Go somewhere
         </a>
       </Card>
-      <Card title='Card another title'>
-        <h6 class='card-subtitle mb-2 text-muted'>Card subtitle</h6>
-        <p class='card-text'>
+      <Card title="Card another title">
+        <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+        <p className="card-text">
           Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+          bulk of the card&#39;s content.
         </p>
-        <a href='#' class='card-link'>
+        <a href="#root" className="card-link">
           Card link
         </a>
-        <a href='#' class='card-link'>
+        <a href="#root" className="card-link">
           Another link
         </a>
       </Card>
--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,16 +1,23 @@
+/* eslint-disable react/require-default-props */
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Card = ({ title, img, children }) => {
-  const image = img || null;
+function Card({ title, img = null, children }) {
   return (
-    <div className='card' style={{width: '18rem'}}>
-      {image ? <img src={image} alt='title' /> : null}
-      <div className='card-body'>
-        <h5 className='card-title'>{title}</h5>
+    <div className="card" style={{ width: '18rem' }}>
+      {img ? <img src={img} alt="title" /> : null}
+      <div className="card-body">
+        <h5 className="card-title">{title}</h5>
         {children}
       </div>
     </div>
   );
+}
+
+Card.propTypes = {
+  title: PropTypes.string.isRequired,
+  img: PropTypes.string,
+  children: PropTypes.node.isRequired,
 };
 
 export default Card;
